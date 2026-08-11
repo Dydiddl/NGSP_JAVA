aca0a54debdc69c86bd92c8be0cf4b652385a4da
dydiddl <dydiddl@naver.com>
2026-08-11 16:31:31 +0900

    [normalizer] docs: document normalization responsibilities

--- src/main/java/normalizer/PersonNormalizer.java
+++ src/main/java/normalizer/PersonNormalizer.java
@@ -1,5 +1,15 @@
 package normalizer;
 
+/**
+ * 사람 등록 및 수정에 사용되는 입력값을
+ * 각 데이터의 규칙에 맞게 정규화한다.
+ *
+ * <p>실제 문자열 변환은 {@link TextNormalizer}의
+ * 공통 정규화 기능을 사용한다.
+ *
+ * <p>이 클래스는 이름, 전화번호, 주소, 계좌번호 등
+ * 사람 데이터의 종류에 따라 어떤 정규화 방법을 사용할지 결정한다.
+ */
 public final class PersonNormalizer {
 
     private PersonNormalizer() {
@@ -21,11 +31,17 @@ public final class PersonNormalizer {
         return TextNormalizer.collapseWhitespace(address);
     }
 
+    // TODO: Bank 별칭 비교시 normalizeKeyword()를 사용할지 검토
     public static String normalizeBank(String bank) {
         return TextNormalizer.removeAllWhitespace(bank);
     }
 
-    public static String normalizeResidentRegistrationNumber(String residentRegistrationNumber) {
-        return TextNormalizer.removeAllWhitespace(residentRegistrationNumber);
+    // TODO: 주민등록번호의 표준 저장 형식을 하이픈 포함/숫자만 중 어떤 형태로 사용할 지 결정한 후 ResidentRegistrationNumber의 genderCode()와 함께 검토
+    public static String normalizeResidentRegistrationNumber(
+            String residentRegistrationNumber
+    ) {
+        return TextNormalizer.removeAllWhitespace(
+                residentRegistrationNumber
+        );
     }
 }
